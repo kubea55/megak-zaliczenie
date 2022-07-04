003fdd545727ec01af2d5bc9b6a4505ca20ee59f
Jakub Burnatowski <k.burnatowski@interia.pl>
2022-07-05 01:43:23 +0200

    added types to the database query results

--- types/task.ts
+++ types/task.ts
@@ -1,9 +1,4 @@
 import {FieldPacket} from "mysql2";
 import {TaskRecord} from "../records/task";
 
-export interface Task {
-    id?: string;
-    task: string
-}
-
 export type TaskRecordResults = [TaskRecord[], FieldPacket[]];
