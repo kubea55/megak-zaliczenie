a839d40689885f3b5e0647b9243a5b5da8cd7aeb
Jakub Burnatowski <k.burnatowski@interia.pl>
2022-07-05 01:30:11 +0200

    Added main router to redirect

--- index.ts
+++ index.ts
@@ -4,11 +4,13 @@ import 'express-async-errors';
 import * as methodOverride from 'method-override';
 import {engine} from 'express-handlebars';
 import {handleError} from "./utils/error";
-import {mainRouter} from "./routers/main";
+import {taskRouter} from './routers/tasks';
+import {mainRouter} from './routers/main';
 
 const app = express();
 
 app.use(methodOverride('_method'));
+
 app.use(eStatic('public'));
 app.use(urlencoded({
     extended: true,
@@ -21,6 +23,7 @@ app.set('view engine', '.hbs');
 app.use(handleError);
 
 app.use('/', (mainRouter))
+app.use('/task', (taskRouter))
 
 
 app.listen(3000, 'localhost', () => {
--- records/task.ts
+++ records/task.ts
@@ -1,41 +1,63 @@
 import {v4 as uuid} from "uuid";
 import {ValidationError} from "../utils/error";
 import {pool} from "../utils/db";
-import {Task, TaskRecordResults} from "../types";
+import {TaskRecordResults} from "../types";
 
 export class TaskRecord {
-    id?: string;
-    task: string;
+    public id?: string;
+    public task: string;
+    public status?: string;
+    public priority: string;
 
-    constructor(obj: Task) {
-        const {task} = obj;
+    constructor(obj: Omit<TaskRecord, 'insert' | 'update' | 'delete'>) {
+        const {id, task, status, priority} = obj;
 
-        if (task.length < 3 || !task || task.length > 250) {
-            throw new ValidationError('Task should be at least 3 and at most 250 characters.')
+        if (task.length < 3 || !task || task.length > 300) {
+            throw new ValidationError('Task should be at least 3 and at most 300 characters.')
         }
+
+        this.id = id;
+        this.task = task;
+        this.status = status ?? "Todo";
+        this.priority = priority;
     };
 
     static async getAll(): Promise<TaskRecord[]> | null {
         const [results] = await pool.execute('SELECT * FROM `tasks`') as TaskRecordResults;
+        // console.log(results.map(obj => new TaskRecord(obj)))
+        // return results.length === 0 ? null : results
         return results.length === 0 ? null : results.map(obj => new TaskRecord(obj));
+    };
 
+    static async getOne(id: string): Promise<TaskRecord> | null {
+        const [results] = await pool.execute('SELECT * FROM `tasks` WHERE `id` = :id', {
+            id
+        }) as TaskRecordResults;
+        console.log(results[0])
+        // console.log(new TaskRecord(results[0]))
+        // return results.length === 0 ? null : results
+        return results.length === 0 ? null : new TaskRecord(results[0]);
     };
 
     async insert(): Promise<string> {
         if (!this.id) {
             this.id = uuid()
         }
-        await pool.execute('INSERT INTO `tasks` (`id`, `task`) VALUES (:id,:task)', {
+        await pool.execute('INSERT INTO `tasks` (`id`, `task`,`status`, `priority`) VALUES (:id,:task,:status,:priority)', {
             id: this.id,
-            task: this.task
+            task: this.task,
+            status: this.status,
+            priority: this.priority,
         });
         return this.id;
     };
 
     async update(): Promise<string> {
-        await pool.execute('UPDATE `tasks` SET `task`= :task WHERE `id` = :id', {
+        await pool.execute('UPDATE `tasks` SET `task`= :task,`status`= :status, `priority`= :priority WHERE `id` = :id', {
             id: this.id,
-            task: this.task
+            task: this.task,
+            status: this.status,
+            priority: this.priority,
         });
         return this.id;
     };
