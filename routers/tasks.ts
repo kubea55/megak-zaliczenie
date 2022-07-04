23fdaec907b2332c5a709f672681d31ea943da14
Jakub Burnatowski <k.burnatowski@interia.pl>
2022-07-05 01:33:34 +0200

    extended handling routers to display all tasks, edit, delete and add new

---
+++ routers/tasks.ts
@@ -0,0 +1,72 @@
+import {Router} from "express";
+import {TaskRecord} from "../records/task";
+
+
+const priority = ["Low", "Medium", "High"]
+const status = ["Todo", "In progress", "Completed"]
+export const taskRouter = Router();
+
+taskRouter
+    .get('/', async (req, res) => {
+        const tasks = (await TaskRecord.getAll()).map((task, index) => {
+            return {
+                numeral: index + 1,
+                task,
+            }
+        })
+        // console.log(tasks)
+        res.render('tasks/all-tasks', {
+            tasks
+        })
+    })
+    .get('/add-form', (req, res) => {
+        res.render('tasks/add-task', {
+            priority
+        })
+    })
+    .get('/:id', (req, res) => {
+        res.render('tasks/one-task')
+    })
+    .get('/edit/:id', async (req, res) => {
+        const id = req.params.id
+        const task = await TaskRecord.getOne(id)
+        // console.log(task)
+        res.render('tasks/edit-task', {
+            task,
+            priority,
+            status
+        })
+    })
+    .post('/add', async (req, res) => {
+        // const {taskName, priority} = req.body
+        const newTask = await new TaskRecord(req.body)
+        const newId = await newTask.insert()
+
+        console.log(newId)
+
+
+        res.render('tasks/added', {
+            newId
+        })
+    })
+    .put('/update/:id', async (req, res) => {
+        const taskId = req.params.id;
+        const obj = req.body
+        const updatedTask = new TaskRecord({...obj, id: taskId})
+        console.log(updatedTask)
+        const id = await updatedTask.update()
+
+
+        res.render('tasks/updated', {
+            id
+        })
+    })
+    .delete('/:id', async (req, res) => {
+        const id = req.params.id
+        const task = await TaskRecord.getOne(id)
+        const deltedId = await task.delete()
+
+        res.render('tasks/deleted', {
+            deltedId
+        })
+    })
